668c4e2e4d4c08b5da76bf779103548ba98cf6ca
ak110dev <ak.apichan1103@gmail.com>
2016-11-23 02:12:29 +0700

    Chore(init)

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>
+            <span>Young</span><br/>
+            <span>Webmaster</span><br/>
+            <span>Calendar</span><br/>
+          </h1>
+        </div>
+        <div className="App-body">
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
